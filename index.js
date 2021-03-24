85c3a91bacef760ec3b4c9750fff9ce0f0f6db85
Jonathan Page <jonnypage-d3@users.noreply.github.com>
2021-03-24 10:53:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('this is feature 1');
 console.log('this is feature 2');
 console.log('this is feature 3', 'this is feature 3a');
-console.log('this is feature 4a, commit 2');
+console.log('this is feature 4a, commit 2'); //Commit 3
